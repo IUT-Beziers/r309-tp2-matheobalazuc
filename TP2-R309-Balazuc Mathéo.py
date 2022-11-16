8a465972f9ea1cbb2f1a3821031fbd1af934a9fe
matheobalazuc <matheo.balazuc@etu.umontpellier.fr>
2022-11-16 11:48:27 +0100

    exercice 1

--- TP2-R309-Balazuc Mathéo.py
+++ TP2-R309-Balazuc Mathéo.py
@@ -16,7 +16,7 @@ fichier = Menu(menuDraweasy)
 menuDraweasy.add_cascade(label="Fichier",menu=fichier)                 # On crée une barre de menu
 fichier.add_command(label="Quitter", command=fenetre1.destroy)         # On ajoute une option au menu
 
-# Création du menu eplacer
+# Création du menu déplacer
 deplacer = Menu(menuDraweasy)
 menuDraweasy.add_cascade(label="Déplacer",menu=deplacer)
 deplacer.add_command(label="Déplacer cet item", command=lambda : tabdeplacer()) 
@@ -28,6 +28,22 @@ effacer.add_command(label="Effacer tout", command=lambda : tabeffacer())
  
 # Afficher le menu
 fenetre1.config(menu=menuDraweasy)
+#------------------------------------------------ Fin de la création menu
     
     
+can1 = Canvas(fenetre1, width =160, height =160) 
+photo = PhotoImage(file ='pc.png') 
+item = can1.create_image(280, 280, image =photo) 
+can2 = Canvas(fenetre1, width =160, height =160) 
+photo = PhotoImage(file ='routeur.png') 
+item = can2.create_image(100, 100, image =photo) 
+can3 = Canvas(fenetre1, width =160, height =160) 
+photo = PhotoImage(file ='switch.png') 
+item = can3.create_image(200, 200, image =photo)
+
+# Mise en page à l'aide de la méthode 'grid' : 
+can1.grid(row =1, column =1, rowspan =3, padx =[0,100], pady =5) 
+can2.grid(row =10, column =1, rowspan =3, padx =[0,100], pady =5) 
+can3.grid(row =20, column =1, rowspan =3, padx =[0,100], pady =5) 
+
 fenetre1.mainloop()
